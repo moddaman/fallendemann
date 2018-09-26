4d4213b54173befa9a0c7d732b22fc7598aa3f39
moddaman <ovrebo90@gmail.com>
2018-09-26 22:32:27 +0200

    Removed npm packages with known vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
   watch: true,
   plugins: [
     definePlugin,
-    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor'/* chunkName= */, filename: 'vendor.bundle.js'/* filename= */}),
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor'/* chunkName= */, filename: 'vendor.bundle.js'/* filename= */ }),
     new HtmlWebpackPlugin({
       filename: '../index.html',
       template: './src/index.html',
